cdcb8e223a77c6bca2a6b8001d34be9575b21890
alaasdk <alaasdk@gmail.com>
2015-01-14 10:47:09 +0200

    Init

---
+++ AssessmentsPOC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AssessmentsPOC.Startup))]
+namespace AssessmentsPOC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
